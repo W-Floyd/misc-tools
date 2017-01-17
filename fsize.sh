984b4c1cda074ef91990b55167c6427a64d6066b
William Floyd <william.png2000@gmail.com>
2017-01-18 09:53:09 +1000

    Added fsize.

---
+++ fsize.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+__filelist=""
+
+__bytes="0"
+
+__usage () {
+echo "${0} <OPTIONS> <FILE(s)>
+
+Prints the file size of all given files
+
+Options:
+  -h  -? --help         This help message
+  -b  --byte            Print the file size in bytes\
+"
+}
+
+if ! [ "${#}" = 0 ]; then
+
+    while ! [ "${#}" = '0' ]; do
+
+        case "${1}" in
+
+            "-h" | "--help" | "-?")
+                __usage
+                exit 0
+                ;;
+
+            "-b")
+                __bytes="1"
+                ;;
+
+            *)
+                if ! [ -e "${1}" ]; then
+                    echo "File \"${1}\" does not exist"
+                else
+                    __filelist="${__filelist}
+${1}"
+                fi
+                ;;
+        esac
+
+        shift
+
+    done
+
+else
+
+    echo "Error: No options passed"
+    exit
+
+fi
+
+echo "${__filelist}" | sed '/^$/d' | sort | uniq | while read __file; do
+
+    if [ "${__bytes}" = '1' ]; then
+
+        echo "${__file} - $(stat "${__file}" -c %s) bytes"
+
+    else
+
+        echo "${__file} - $(stat "${__file}" -c %s | numfmt --to=iec-i)"
+
+    fi
+
+done
+
+exit
