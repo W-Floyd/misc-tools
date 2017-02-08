d55b307ec4c8ee7892823f92295cc8b3886468ad
William Floyd <william.png2000@gmail.com>
2017-02-08 21:16:11 +1000

    Added mext, odir and oext.

---
+++ mext.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# <LIST_OF_FILES> | mext <FILE_1> <FILE_2> <FILE_3> ...
+#
+# Trims all text after the last '.' in any given strings,
+# either piped, and/or as inputs
+#
+
+__mext () {
+echo "${1}" | sed 's|\(.*\)\(\.\)\(.*\)|\3|'
+}
+
+while ! [ "${#}" = '0' ]; do
+    __mext "${1}"
+    shift
+done
+
+if read -t 0; then
+    cat | while read -r __value; do
+        __mext "${__value}"
+    done
+fi
+
+exit
---
+++ oext.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# <LIST_OF_FILES> | oext <FILE_1> <FILE_2> <FILE_3> ...
+#
+# Trims all text before the last '.' in any given strings,
+# either piped, and/or as inputs
+#
+
+__oext () {
+echo "${1}" | sed 's|\(.*\)\(\.\)\(.*\)|\3|'
+}
+
+while ! [ "${#}" = '0' ]; do
+    __oext "${1}"
+    shift
+done
+
+if read -t 0; then
+    cat | while read -r __value; do
+        __oext "${__value}"
+    done
+fi
+
+exit
