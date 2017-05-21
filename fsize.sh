2bf99bb18c2c7a254c418b772174705000e9aad8
William Floyd <william.png2000@gmail.com>
2017-05-21 23:56:27 +1000

    fsize: Allow joined short options, flag to show total size, other fixes.
    Options may now be given like so: '-sbt', rather than '-s -b -t'.
    
    '-t' option added, which enables a final report of total sizes of all
    files given, also respecting '-b' option.
    
    Signed-off-by: William Floyd <william.png2000@gmail.com>

--- fsize.sh
+++ fsize.sh
@@ -12,6 +12,8 @@ __sort_size='0'
 
 __reverse='0'
 
+__total='0'
+
 __usage () {
 echo "${0} <OPTIONS> <FILE(s)>
 
@@ -22,7 +24,8 @@ Options:
   -b   --byte            Print the file size in bytes
   -n   --name            Sort the files in order of name
   -s   --size            Sort the files in order of size
-  -r   --reverse         Reverse the order of the listed files\
+  -r   --reverse         Reverse the order of the listed files
+  -t   --total           Print total size of listed files\
 "
 }
 
@@ -30,47 +33,80 @@ __size () {
 stat "${1}" -c %s
 }
 
+__check_input () {
+
+case "${1}" in
+
+    "h" | "--help" | "?")
+        __usage
+        exit 0
+        ;;
+
+    "b" | "--byte")
+        __bytes="1"
+        ;;
+
+    "n" | "--name")
+        __sort_size='0'
+        __sort_name='1'
+        ;;
+
+    "s" | "--size")
+        __sort_size='1'
+        __sort_name='0'
+        ;;
+
+    "r" | "--reverse")
+        __reverse='1'
+        ;;
+
+    "t" | "--total")
+        __total='1'
+        ;;
+
+    *)
+        echo "Invalid option \"${1}\" passed."
+        exit
+        ;;
+esac
+
+}
+
+__process_option () {
+
+if grep '^--.*' <<< "${1}" &> /dev/null; then
+
+    __check_input "${1}"
+
+elif grep '^-.*' <<< "${1}" &> /dev/null; then
+
+    __letters="$(echo "${1}" | cut -c 2- | sed 's/./& /g')"
+
+    for __letter in ${__letters}; do
+
+        __check_input "${__letter}"
+
+    done
+
+else
+    __files_specified='1'
+    if ! [ -e "${1}" ]; then
+        echo "File \"${1}\" does not exist "
+    elif ! [ -r "${1}" ]; then
+        echo "File \"${1}\" is not readable"
+    elif ! [ -d "${1}" ]; then
+        __filelist="${__filelist}
+${1}"
+    fi
+fi
+
+}
+
 if ! [ "${#}" = 0 ]; then
 
     while ! [ "${#}" = '0' ]; do
 
-        case "${1}" in
-
-            "-h" | "--help" | "-?")
-                __usage
-                exit 0
-                ;;
-
-            "-b" | "--byte")
-                __bytes="1"
-                ;;
-
-            "-n" | "--name")
-                __sort_size='0'
-                __sort_name='1'
-                ;;
-
-            "-s" | "--size")
-                __sort_size='1'
-                __sort_name='0'
-                ;;
-
-            "-r" | "--reverse")
-                __reverse='1'
-                ;;
-
-            *)
-                __files_specified='1'
-                if ! [ -e "${1}" ]; then
-                    echo "File \"${1}\" does not exist"
-                elif ! [ -r "${1}" ]; then
-                    echo "File \"${1}\" is not readable"
-                elif ! [ -d "${1}" ]; then
-                    __filelist="${__filelist}
-${1}"
-                fi
-                ;;
-        esac
+        __process_option "${1}"
 
         shift
 
@@ -89,9 +125,13 @@ ${__file}"
 
 fi
 
-if [ -z "${__filelist}" ] && [ "${__files_specified}" = '0' ]; then
-    echo "Error: No readable files found"
-    exit 1
+if [ -z "${__filelist}" ]; then
+    if [ "${__files_specified}" = '0' ]; then
+        __filelist="$(find . -maxdepth 1 -type f | sed 's/^\.\///' | sort)"
+    else
+        echo "No valid files listed."
+        exit
+    fi
 fi
 
 __filelist="$(sed '/^$/d' <<< "${__filelist}")"
@@ -141,5 +181,24 @@ else
 fi
 
 )
+
+if [ "${__total}" = '1' ]; then
+    echo
+
+    __total_size="$(echo "${__filelist}" | sed '/^$/d' | while read -r __file; do
+        echo -n "$(__size "${__file}")+"
+    done | sed 's/+$/\n/' | bc)"
+
+    if [ "${__bytes}" = '1' ]; then
+
+        echo "${__total_size} bytes - Total"
+
+    else
+
+        echo "$(echo ${__total_size} | numfmt --to=iec-i) - Total"
+
+    fi
+
+fi
 
 exit
