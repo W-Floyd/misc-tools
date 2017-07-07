a2a29c87e1eeee515aa106f3203bf3524c6bcb85
William Floyd <william.png2000@gmail.com>
2017-07-08 09:22:27 +1000

    raproximate: Complete rewrite, no known bugs.
    Signed-off-by: William Floyd <william.png2000@gmail.com>

--- raproximate.sh
+++ raproximate.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-# Ratio Approximator
-#
-# raproximate <DECIMAL_NUMBER> -c
-#
-# Press enter to keep trying to find closer approximations
-# or use -c to not break and only show best
+set -e
 
-if ! [ -z "$(echo "${1}" | sed -e 's/[0-9]*\.[0-9]*//' -e 's/[0-9]*//')" ]; then
-    echo "Not a number"
-    exit
-fi
+declare -A __time __time_description __time_start __script_option
 
 stripzero () {
-cat | sed -e 's/\([^0]*\)0*$/\1/' -e 's/\.$//'
+#cat | sed -e 's/\(\.[0-9]*[^0]\)0*$/\1/' -e 's/\.$//' -e 's/\(.*[^0]\)[0*|\.0*]$/\1/'
+cat | sed -e 's/^0*\(.*\..*\)/\1/' -e 's/\(.*\..*[^0]\)0*/\1/' -e 's/\.0*$//'
 }
 
 hcf () {
@@ -24,6 +17,9 @@ if [[ $1 -eq $2 ]];then
 elif [[ $1 -gt $2 ]];then
    greater="${1}"
    lower="${2}"
+elif [[ $1 -eq 0 ]];then
+    echo '1'
+    return 0
 else
    greater="${2}"
    lower="${1}"
@@ -40,106 +36,256 @@ echo "${hcf}"
 }
 
 simplify () {
-# simplify a b
+# simplify a b, outputs in a:b
 local common="$(hcf "${1}" "${2}")"
 
-local __a="$(echo "${1}/${common}" | bc)"
-local __b="$(echo "${2}/${common}" | bc)"
+local __a="$(bc <<< "${1}/${common}")"
+local __b="$(bc <<< "${2}/${common}")"
 
-echo "${__a} ${__b}"
+echo "${__a}:${__b}"
 }
 
-__goal="${1}"
-__ratios=''
-__tried=''
-__diffs=''
-__best=''
-_a="$(echo "${__goal}/1" | bc)"
-_b='1'
+__usage () {
+echo "$(basename "${0}") <OPTIONS> <NUMBER>
 
-while true; do
+Approximates ratios to a given number until it finds an exact match.
 
-    __ratio="$(echo "${_a}/${_b}" | bc -l)"
+Options:
+  -h  -?  --help            Short help (this message).
 
-    __ratios="${_a}:${_b}
-${__ratios}"
+  -c  --converge            Use non-exhaustive starting numbers that may
+                            converge quicker with numbers larger than 10
 
-    __tried="${__ratio}
-${__tried}"
+  -e  --exact               Use an alternative method to find the exact ratio.
 
-    if [ "$(echo "${__ratio} > ${__goal}" | bc)" = '1' ]; then
-        __sign='+'
-        __diff="$(echo "(${__ratio}/${__goal})-1" | bc -l | stripzero)"
-    else
-        __sign='-'
-        __diff="$(echo "1-(${__ratio}/${__goal})" | bc -l | stripzero)"
-    fi
+  -b  --benchmark           Time some operations and give an iteration reading.
+                            Comes with significant performance issues."
+}
 
-    __diffs="${__sign}${__diff}
-${__diffs}"
+# __set_option <NAME> <VAL>
+__set_option () {
+__script_option[${1}]="${2}"
+}
 
-    if ! [ "${2}" = '-c' ]; then
+__check_option () {
+[[ "${__script_option[${1}]}" = '1' ]]
+}
 
-    echo -n "$(simplify ${_a} ${_b} | tr ' ' ':') - $(echo "${__ratio}" | stripzero) - "
+__print_option () {
+echo "${1^}: ${__script_option[${1}]}"
+}
 
-    if [ "${__sign}" = '-' ]; then
-        echo -e "\e[31m-${__diff}\e[39m"
-    else
-        echo -e "\e[32m+${__diff}\e[39m"
-    fi
+__set_option converge 0
+__set_option exact 0
+__set_option benchmark 0
 
-    fi
+until [ "${#}" = '0' ]; do
 
-    __best="$(echo "${__diffs}" | grep -v '^$' | cut -c '2-' | sort -n | head -n 1)"
+    case "${1}" in
 
-    __match_line="$(echo "${__diffs}" | cut -c '2-' | grep -n "${__best}" | head -n 1 | sed 's/\([0-9]*\):.*/\1/')"
-
-    __best_ratio="$(echo "${__ratios}" | sed "${__match_line}!d")"
-
-    if [ "$(echo "$(echo ${__best_ratio} | sed 's#:#/#' | bc -l) > ${__goal}" | bc)" = '1' ]; then
-        __best_sign='+'
-    else
-        __best_sign='-'
-    fi
-
-    if [ "${2}" = '-c' -a "${__best}" = "${__diff}" ]; then
-
-    echo -n "Best so far: $(simplify "$(echo ${__best_ratio} | sed 's/:.*//')" "$(echo ${__best_ratio} | sed 's/.*://')" | tr ' ' ':') - "
-
-    if [ "${__best_sign}" = '-' ]; then
-        echo -e "\e[31m-${__best}\e[39m"
-    else
-        echo -e "\e[32m+${__best}\e[39m"
-    fi
-
-    if [ "${__best}" = '0' ]; then
-        echo "
-Exactly: $(simplify "$(echo ${__best_ratio} | sed 's/:.*//')" "$(echo ${__best_ratio} | sed 's/.*://')" | tr ' ' ':')"
-
+    '-h' | '-?' | '--help')
+        __usage
         exit
-    fi
+        ;;
 
-    fi
+    '-c' | '--converge')
+        __set_option converge 1
+        __exact='0'
+        ;;
 
-    if ! [ "${__best}" = "${__last_best}" ] && ! [ "${2}" = '-c' ]; then
-        __last_best="${__best}"
-        read -n 1
-    elif ! [ "${2}" = '-c' ]; then
-        echo
-    fi
+    '-e' | '--exact')
+        __set_option exact 1
+        ;;
 
-    until ! echo "${__tried}" | grep "${__ratio}" &> /dev/null; do
+    '-b' | '--benchmark')
+        __set_option benchmark 1
+        ;;
 
-        if [ "${__sign}" = '-' ]; then
-            _a=$((_a+1))
-        else
-           _b=$((_b+1))
-        fi
+    '-'*)
+        echo 'Not an option'
+        exit
+        ;;
 
-        __ratio="$(echo "${_a}/${_b}" | bc -l)"
+    *)
+        __goal="$(bc -l <<< ${1} | stripzero)"
+        ;;
 
-    done
+    esac
+
+    shift
 
 done
+
+if [ -z "${__goal}" ]; then
+    echo 'No goal specified'
+    exit
+elif ! [ -z "$(sed -e 's/[0-9]\+\.[0-9]\+//' -e 's/\.[0-9]\+//' -e 's/[0-9]*//' <<< "${__goal}")" ]; then
+    echo "Not a number"
+    exit
+fi
+
+__set_ratio () {
+__ratio="$(bc -l <<< "${_a}/${_b}" | stripzero)"
+}
+
+__set_diff () {
+__diff="$(bc -l <<< "${__goal}-${__ratio}" | sed 's/^[-|+]//' | stripzero)"
+if [ -z "${__diff}" ]; then
+    __diff='0'
+fi
+}
+
+__set_sign () {
+if [ "${__ratio}" = "${__goal}" ]; then
+    __sign=''
+elif [ "$(bc -l <<< "${__ratio} > ${__goal}")" = '1' ]; then
+    __sign='+'
+else
+    __sign='-'
+fi
+}
+
+__check_best () {
+if [ "$(bc -l <<< "${__diff#[+|-]} < ${__best_diff#[+|-]}" 2> /dev/null)" = '1' ] || ( [ -z "${__best_diff}" ] || [ -z "${__best_ratio}" ] ); then
+    __best_diff="${__sign}${__diff}"
+    __best_ratio="${_a}:${_b}"
+    if [ "${__best_diff}" = '0' ]; then
+        echo "$(simplify "${__best_ratio/:*}" "${__best_ratio/*:}") - Exactly"
+        break
+    fi
+    echo -n "$(simplify ${_a} ${_b}) - ${__ratio} - "
+    if [ "${__sign}" = '-' ]; then
+        echo -e "\e[34m-${__diff}\e[39m"
+    else
+        echo -e "\e[36m+${__diff}\e[39m"
+    fi
+fi
+}
+# __timer start|end DESCRIPTION
+__timer () {
+if __check_option benchmark; then
+    local __curtime="$(date +%s.%N)"
+    local __hash="$(md5sum <<< "${2}" | sed -e 's/ .*//' -e 's/^/X/')"
+    if [ -z "${__time[${__hash}]}" ]; then
+        __time[${__hash}]='0'
+    fi
+    if [ -z "${__time_description[${__hash}]}" ]; then
+        __time_description[${__hash}]="${2}"
+    fi
+    if [ "${1}" = 'start' ]; then
+        __time_start[${__hash}]="${__curtime}"
+    elif [ "${1}" = 'end' ]; then
+        __time[${__hash}]="$(bc <<< "${__time[${__hash}]}+(${__curtime}-${__time_start[${__hash}]})")"
+    else
+        echo 'Timer derp'
+    fi
+fi
+}
+
+__get_time () {
+local __hash="$(md5sum <<< "${1}" | sed -e 's/ .*//' -e 's/^/X/')"
+echo "${__time_description[${__hash}]^}: ${__time[${__hash}]}"
+}
+
+i=0
+
+#set -x
+
+if __check_option exact; then
+
+    __timer start "Calculating exact number"
+
+    i=$((i+1))
+
+    if [ "${__goal}" = '0' ]; then
+
+        __real='0:1'
+
+    else
+
+        __decimal="${__goal#[0-9]*.}"
+
+        __mult="$(bc <<< "10^${#__decimal}")"
+
+        _a="$(bc -l <<< "${__goal}*${__mult}" | stripzero)"
+
+        _b="${__mult}"
+
+        __real="$(simplify "${_a}" "${_b}")"
+
+    fi
+
+    echo "${__real/:*}:${__real/*:} - Exactly"
+
+    __timer end "Calculating exact number"
+
+else
+
+    _a="$(bc <<< "${__goal}/1")"
+    if [ "${__converge}" = '1' ]; then
+        __num="$(bc <<< "10^(${#_a}-1)")"
+        _b="${__num}"
+        _a="$(bc <<< "${_a}*${__num}")"
+    else
+        _b='1'
+    fi
+
+    if [ "${__goal}" = '0' ]; then
+
+        echo "0:1 - Exactly"
+
+    else
+
+        while true; do
+
+            i=$((i+1))
+
+            #__timer start "Set ratio"
+            __set_ratio
+            #__timer end "Set ratio"
+
+            #__timer start "Set diff"
+            __set_diff
+            #__timer end "Set diff"
+
+            #__timer start "Set sign"
+            __set_sign
+            #__timer end "Set sign"
+
+            #__timer start "Check best"
+            __check_best
+            #__timer end "Check best"
+
+            #__timer start "Change ratio"
+
+            if [ "${__sign}" = '+' ]; then
+                _b=$((_b+1))
+            else
+                _a=$((_a+1))
+            fi
+
+            #__timer end "Change ratio"
+
+        done
+
+    fi
+
+fi
+
+if __check_option benchmark; then
+
+    for __item in "${__time_description[@]}"; do
+        __get_time "${__item}"
+    done | sort -rgk 3
+
+    echo -n 'Total: '
+
+    for __item in "${__time_description[@]}"; do
+        __get_time "${__item}"
+    done | sed 's/.* //' | paste -sd+ | bc
+
+    echo "${i} Iterations"
+
+fi
 
 exit
